bd99a99e47e0f60827cba64ac8548bfbfbdbf940
Yiping-Dong <dongy551@newschool.edu>
2021-10-14 14:34:28 -0400

    javascript change svg color

---
+++ ugly-website/change_color.js
@@ -0,0 +1,8 @@
+document.getElementById("refill").addEventListener("click", changeColor);
+var melon = document.getElementById("refill");
+
+// Click on the melon to change the color of it!
+function changeColor(){
+     melon.setAttribute("style", "fill: rgb(255,240,70)");
+     }
+    
