3ad13b39524706d690095d37872376707a31d055
Yiping-Dong <dongy551@newschool.edu>
2021-11-15 18:01:16 -0500

    responsive ugly website

--- ugly-website/change_color.js
+++ ugly-website/change_color.js
@@ -3,6 +3,9 @@ var melon = document.getElementById("refill");
 
 // Click on the melon to change the color of it!
 function changeColor(){
+     if (melon.style.fill === "red"){
      melon.setAttribute("style", "fill: rgb(255,240,70)");
+     }else{
+     melon.setAttribute("style", "fill: red");
      }
-    
+}
